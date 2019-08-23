2b934704780b955dcc795ce8acfa880066a6480c
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-08-23 13:02:59 +0100

    optimise solution

--- src/main/java/Leetcode389.java
+++ src/main/java/Leetcode389.java
@@ -1,20 +1,15 @@
 class Solution {
     
     public char findTheDifference(String s, String t) {
+        char ret = 0;
         int[] arr = new int[26];
         for (char c : s.toCharArray()) {
             arr[c - 'a']++;
         }
         
-        int[] arr2 = new int[26];
         for (char c : t.toCharArray()) {
-            arr2[c - 'a']++;
-        }
-        
-        char ret = 'a';
-        for (int i = 0; i < 26; i++) {
-            if (arr2[i] != arr[i]) {
-                ret += i;
+            if (--arr[c - 'a'] < 0) {
+                ret += c;
                 break;
             }
         }
