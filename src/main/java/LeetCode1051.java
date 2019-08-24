5c5616117890e8b35a53fc6dd7fa8d1e00804dfe
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-08-24 09:40:44 +0100

    Create LeetCode1051.java

---
+++ src/main/java/LeetCode1051.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    public int heightChecker(int[] heights) {
+        int[] indexes = new int[heights.length];
+        for (int i = 0; i < heights.length - 1; i++) {
+            for (int j = heights.length - 1; j > i; j--) {
+                if (heights[i] > heights[j]) {
+                    int temp = heights[i];
+                    heights[i] = heights[j];
+                    heights[j] = temp;
+                    indexes[i] = 1;
+                    indexes[j] = 1;
+                }
+            }
+        }
+        
+        return Arrays.stream(indexes).sum();
+    }
+}
