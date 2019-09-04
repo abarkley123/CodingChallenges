fc3c438e27ddcf6eac8beb5a17d519345f2c9fda
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-04 22:48:13 +0100

    Create LeetCode530.java

---
+++ src/main/java/LeetCode530.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public int getMinimumDifference(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root, list);
+        Collections.sort(list);
+
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 1; i < list.size(); i++) minDiff = Math.min(minDiff, list.get(i) - list.get(i - 1));
+        
+        return minDiff;
+    }
+    
+    public void dfs(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        
+        list.add(node.val);
+        dfs(node.left, list);
+        dfs(node.right, list);
+    }
+}
