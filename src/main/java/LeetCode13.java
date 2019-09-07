110bbd1a13224ed4c3d2bfe6e34811ea94868128
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-07 10:51:32 +0100

    Create LeetCode13.java

---
+++ src/main/java/LeetCode13.java
@@ -0,0 +1,41 @@
+class Solution {
+    
+    private static final Map<String, Integer> values = new HashMap<>();
+    
+    static {
+        values.put("I", 1);
+        values.put("IV", 4);
+        values.put("V", 5);
+        values.put("IX", 9);
+        values.put("X", 10);
+        values.put("XL", 40);
+        values.put("L", 50);
+        values.put("XC", 90);
+        values.put("C", 100);
+        values.put("CD", 400);
+        values.put("D", 500);
+        values.put("CM", 900);
+        values.put("M", 1000);
+    }
+    
+    public int romanToInt(String s) {
+        int ret = 0, len = 0;
+        if (s == null || (len = s.length()) == 0) return ret;
+        
+        for (int i = 0; i < len; i++) {
+            int val = values.get(String.valueOf(s.charAt(i)));
+            if (i == len - 1) {
+                ret += val;
+            } else {
+                Integer tmpInt = 0;
+                String tmp = s.substring(i, i + 2);
+                if ((tmpInt = values.get(tmp)) != null) {
+                    ret += tmpInt;
+                    i++;
+                } else ret += val;
+            }
+        }
+                
+        return ret;
+    }
+}
