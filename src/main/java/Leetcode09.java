c4c080abacf56698ae465aa9fb3d8e455e89b3eb
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-08 10:35:12 +0100

    Update Leetcode09.java
    Update to use more efficient two-pointer solution

--- src/main/java/Leetcode09.java
+++ src/main/java/Leetcode09.java
@@ -3,15 +3,11 @@ class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
         
-        Stack<Character> st = new Stack<>();
         char[] arr = Integer.toString(x).toCharArray();
         
-        for (int i = 0; i < arr.length/2; i++) {
-            st.push(arr[i]);
-        }
-        
-        for (int j = arr.length - arr.length/2; j < arr.length; j++) {
-            if (arr[j] != st.pop()) return false;
+        int i = 0, j = arr.length - 1;
+        while (i <= arr.length/2 && j >= arr.length/2) {
+            if (arr[i++] != arr[j--]) return false;
         }
         
         return true;
