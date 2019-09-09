60a6d021a280b31f127595d761e1e9291411f9d0
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-09 10:25:41 +0100

    Two pointer solution

--- src/main/java/Leetcode26.java
+++ src/main/java/Leetcode26.java
@@ -3,26 +3,14 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         
-        int ret = 0, i = 0;
-        while (i < nums.length - ret - 1) {
-            if (nums[i] == nums[i + 1]) {
-                ret++;
-                nums[i + 1] = Integer.MAX_VALUE;
-            } else if (nums[i] == Integer.MAX_VALUE || nums[i + 1] == Integer.MAX_VALUE) {
-                sortForNextValue(i, nums);  
-                sortForNextValue(i + 1, nums);                  
-            } else i++;
+        int i = 0, j = 1;
+        for (; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
         }
         
-        return nums.length - ret;
-    }
-    
-    private void sortForNextValue(int i, int[] nums) {
-        int j = i;
-        while (nums[j] == Integer.MAX_VALUE) j++;
-        if (j != i) {
-            nums[i] = nums[j];
-            nums[j] = Integer.MAX_VALUE;   
-        }
+        return i + 1;
     }
 }
