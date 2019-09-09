f30447aba2e465d9818f597bcaba41acc3dd24bd
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-09 10:17:24 +0100

    sort array inplace

--- src/main/java/Leetcode26.java
+++ src/main/java/Leetcode26.java
@@ -8,10 +8,21 @@ class Solution {
             if (nums[i] == nums[i + 1]) {
                 ret++;
                 nums[i + 1] = Integer.MAX_VALUE;
-                Arrays.sort(nums);
+            } else if (nums[i] == Integer.MAX_VALUE || nums[i + 1] == Integer.MAX_VALUE) {
+                sortForNextValue(i, nums);  
+                sortForNextValue(i + 1, nums);                  
             } else i++;
         }
         
         return nums.length - ret;
+    }
+    
+    private void sortForNextValue(int i, int[] nums) {
+        int j = i;
+        while (nums[j] == Integer.MAX_VALUE) j++;
+        if (j != i) {
+            nums[i] = nums[j];
+            nums[j] = Integer.MAX_VALUE;   
+        }
     }
 }
