1a11f2ef8ed442faf0f835e82112e023a916cdc1
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-13 09:57:10 +0100

    long solution

--- src/main/java/Leetcode07.java
+++ src/main/java/Leetcode07.java
@@ -1,20 +1,16 @@
 class Solution {
     
     public int reverse(int x) {
-        if (x == 0) return x;
-        
-        String sb = x < 0 ? "-" : "";
-        int start = x < 0 ? 1 : 0, charsAdded = 0;
-        String s = Integer.toString(x).substring(start);
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (charsAdded == 0 && s.charAt(i) != '0' || charsAdded > 0) {
-                charsAdded++;
-                sb += s.charAt(i);
-            } 
+        long ret = 0;
+        while (x != 0) {
+            ret = ret * 10 + x % 10;
+            x /= 10;
         }
         
-        double d = Double.parseDouble(sb);
-        return Math.abs(d) > Integer.MAX_VALUE ? 0 : (int) d;
+        if (Math.abs(ret) > Integer.MAX_VALUE) {
+            return 0;
+        } else {
+            return (int) ret;
+        }
     }
 }
