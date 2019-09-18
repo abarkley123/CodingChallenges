9659c3867e96767295f6a688b0b81e9733559486
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-18 09:52:05 +0100

    Update Leetcode125.java

--- src/main/java/Leetcode125.java
+++ src/main/java/Leetcode125.java
@@ -47,13 +47,13 @@ class Solution {
         int i = 0, j = s.length() - 1;
         
         while (i < j) {
-            if (!Character.isLetterOrDigit(s.charAt(i))) i++;
-             else if (!Character.isLetterOrDigit(s.charAt(j))) j--;
-             else {
-                if (Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))) return false;
-             }
+             if (!isLetterOrDigit(s.charAt(i))) i++;
+             else if (!isLetterOrDigit(s.charAt(j))) j--;
+             else if (toLowerCase(s.charAt(i++)) != toLowerCase(s.charAt(j--))) return false;
         }
         
         return true;
     }
+    
+    -- static imports are implied --
 }
