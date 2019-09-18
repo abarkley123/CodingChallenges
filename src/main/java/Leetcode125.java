49c2dd098f2b6bfaf0f81294a67777527e19bf86
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-18 09:47:04 +0100

    Update Leetcode125.java

--- src/main/java/Leetcode125.java
+++ src/main/java/Leetcode125.java
@@ -41,4 +41,19 @@ class Solution {
         s = s.replaceAll("[^a-zA-Z0-9]", "");                                    
         return s.equalsIgnoreCase(new StringBuilder(s).reverse().toString());
     }
+    
+    /** Solution using pure loops and no regex - 4ms 37.2mb **/
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        
+        while (i < j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) i++;
+             else if (!Character.isLetterOrDigit(s.charAt(j))) j--;
+             else {
+                if (Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))) return false;
+             }
+        }
+        
+        return true;
+    }
 }
