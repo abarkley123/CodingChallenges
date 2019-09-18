23dcef7debb69e322f9e62442b38b25d02e225f8
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-09-18 09:41:42 +0100

    Add alternative solutions

--- src/main/java/Leetcode125.java
+++ src/main/java/Leetcode125.java
@@ -4,6 +4,7 @@ class Solution {
     
     private static final Pattern pattern = Pattern.compile("^[a-zA-Z0-9]*$");
     
+    /** Pattern matching solution - 21ms 37.7mb **/
     public boolean isPalindrome(String s) {
         String str = s.toLowerCase();
         int len = s.length(), i = 0, j = len - 1, tmp = 0;
@@ -21,5 +22,23 @@ class Solution {
         }
         
         return true;
+    }
+    
+    /** Two pointer solution, with one regex check - 19ms 38.7mb **/
+    public boolean isPalindrome(String s) {
+        String str = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");                                      
+        int len = str.length(), i = 0, j = len - 1;
+        
+        while (i < j) {
+            if (str.charAt(i++) != str.charAt(j--)) return false;
+        }
+        
+        return true;
+    }
+    
+    /** An even more concise solution - 23ms 37.7mb **/
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "");                                    
+        return s.equalsIgnoreCase(new StringBuilder(s).reverse().toString());
     }
 }
