0440f42fea6a068cd36d9bb6a1a032374e9aae55
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-10-05 10:35:39 +0100

    add more efficient solution

--- src/main/java/Leetcode414.java
+++ src/main/java/Leetcode414.java
@@ -1,5 +1,30 @@
 class Solution {
     
+    /** O(n) three-variable-store: 0ms, 37.4MB **/
+    public int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE, second = Long.MIN_VALUE, third = Long.MIN_VALUE;
+		
+        for (int i = 0; i < nums.length; i++) {
+		// if we find a new max, we must shift first -> second, and second -> third.
+            if (nums[i] > first) {
+                long tmp = first;
+                long tmp2 = second;
+                first = nums[i];
+                second = tmp;
+                third = tmp2;
+		// otherwise if we find a secondary max, we must shift the original second -> third.
+            } else if (nums[i] > second && nums[i] != first) {
+                long tmp = second;
+                second = nums[i];
+                third = tmp;
+		// otherwise if we find a 'unique' third maximum, assign it to the third variable.
+            } else if (nums[i] > third && nums[i] != second && nums[i] != first) third = nums[i];
+        }
+        
+		// if we found fewer than 3 distinct values, return the largest. Otherwise return as normal.
+        return (int) (third == Long.MIN_VALUE ? first : third);
+    }
+    
     /** check for distinct maxes: 2ms, 36.5MB **/
     public int thirdMax(int[] nums) {
         Arrays.sort(nums);
