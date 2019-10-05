7c799fa13aa2c61d31f235bb35aa5cdc8c37a146
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-10-05 10:42:49 +0100

    climbing stairs

---
+++ src/main/java/Leetcode70.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    /** Simple dp soln - 0ms (100%), 32.7MB (5.76%) **/ 
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+}
