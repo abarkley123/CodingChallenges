3eb0c70bb800dad38b70d8ea41242136d6c9fbc3
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-10-06 10:06:49 +0100

    add hashmap solution

--- src/main/java/Leetcode506.java
+++ src/main/java/Leetcode506.java
@@ -44,4 +44,24 @@ class Solution {
         
         return ret;
     }
+    
+    /** Solution using hashmap - O(N): 6ms (83%), 37.4MB (100%) **/
+    public String[] findRelativeRanks(int[] nums) {
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) arr[i] = nums[i];
+        Arrays.sort(arr);
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int j = 0; j < arr.length; j++) map.put(arr[j], nums.length - j);   
+        
+        String[] ret = new String[arr.length];
+        for (int k = 0; k < arr.length; k++) {
+            if (nums[k] == arr[arr.length - 1]) ret[k] = "Gold Medal";
+            else if (nums[k] == arr[arr.length - 2]) ret[k] = "Silver Medal"; 
+            else if (nums[k] == arr[arr.length - 3]) ret[k] = "Bronze Medal";
+            else ret[k] = map.get(nums[k]) + "";
+        }
+        
+        return ret;
+    }
 }
