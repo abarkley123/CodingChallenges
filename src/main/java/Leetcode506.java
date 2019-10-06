cc09daf90f071ff2de48bf8b00faa743bb868463
Andrew Barkley <30977884+abarkley123@users.noreply.github.com>
2019-10-06 09:45:42 +0100

    relative ranks

---
+++ src/main/java/Leetcode506.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    /** O(N^2) solution: 70ms (5.44%), 40.1MB (100%) **/
+    /** Note: Using Integer and a comparator to sort likely incurs a heavy peformance penalty **/
+    public String[] findRelativeRanks(int[] nums) {
+        Integer[] arr = new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) arr[i] = nums[i];
+        Arrays.sort(arr, Collections.reverseOrder());
+        
+        final int len = nums.length;
+        String[] ret = new String[len];
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == arr[0]) ret[i] = "Gold Medal";
+            else if (nums[i] == arr[1]) ret[i] = "Silver Medal"; 
+            else if (nums[i] == arr[2]) ret[i] = "Bronze Medal";
+            else {
+                int j = 0;
+                while (arr[j] != nums[i]) j++;
+                ret[i] = Integer.toString(j + 1);   
+            }
+        }
+        
+        return ret;
+    }
+}
